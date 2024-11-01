452d0bc00f73cd728e03e21796da3d0fc170fa01
Akshay Jha <akshayjha2006@gmail.com>
2024-11-02 00:00:47 +0530

    done with oval

---
+++ src/components/FirstMainBox.tsx
@@ -0,0 +1,12 @@
+import OvalShape from "./Oval";
+const FirstMainBox = () => {
+  return (
+    <div
+      className={`bg-[#171717] overflow-hidden no-scrollbar w-full h-screen relative`}
+    >
+      <OvalShape />
+    </div>
+  );
+};
+
+export default FirstMainBox;
---
+++ src/components/Oval.tsx
@@ -0,0 +1,8 @@
+const OvalShape = () => {
+  return (
+    <div
+      className={`w-[200%] aspect-square no-scrollbar rounded-full bg-[#212121] absolute top-[100%] -translate-y-[4.5%] translate-x-[-25%]`}
+    ></div>
+  );
+};
+export default OvalShape;
